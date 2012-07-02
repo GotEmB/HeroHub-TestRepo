459653918e1589dc93cf04e66f2dc7cec26c7719
Gautham Badhrinathan <b.gautham@gmail.com>
2012-07-02 18:20:08 +0400

    Basic Node App.

---
+++ web.js
@@ -0,0 +1,6 @@
+http = require("http");
+
+server = http.createServer();
+
+server.listen((port = process.env.port ? process.env.port : 5000), function() {console.log("Listening on port " + port);})
+
