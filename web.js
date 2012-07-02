e59fd08178d4291a51ac0dda9b09425ccbdb02bb
Gautham Badhrinathan <b.gautham@gmail.com>
2012-07-02 18:54:32 +0400

    Update web.js #deploy

--- web.js
+++ web.js
@@ -2,5 +2,8 @@ http = require("http");
 
 server = http.createServer();
 
-server.listen((port = process.env.port ? process.env.port : 5000), function() {console.log("Listening on port " + port);})
-
+server.listen((port = process.env.port ? process.env.port : 5000), function()
+{
+  console.log("Listening on port " + port);
+  
+});
