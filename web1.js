527b4b001e2846b2118bbe034de7077ff177a886
Gautham Badhrinathan <b.gautham@gmail.com>
2012-07-12 20:33:57 +0400

    bleh #deploy

--- web1.js
+++ web1.js
@@ -2,7 +2,7 @@ http = require("http");
 
 server = http.createServer();
 
-server.listen((port = process.env.port || 5000), function()
+server.listen((port = process.env.port ? process.env.port : 5000), function()
 {
   console.log("Listening on port " + port);
   
